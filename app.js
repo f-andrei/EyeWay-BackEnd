88e10f909988c0900a7f97cd34750c3f01c15049
KaiquePerciliano <kaiqueperciliano374@gmail.com>
2024-08-31 18:39:25 -0300

    add app js

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const userRoutes = require('./routes/users');
+const cameraRoutes = require('./routes/cameras');
+const infractionRoutes = require('./routes/infractions');
+const infractionsStatsRoutes = require('./routes/infractionsStats');
+
+
+app.use('/users', userRoutes);
+app.use('/cameras', cameraRoutes);
+app.use('/infractions', infractionRoutes);
+app.use('/infractions-stats', infractionsStatsRoutes);
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
