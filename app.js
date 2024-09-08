79d3f908c5ea3a30d7987e88cbb17d799b3f0d3c
Andrei <andreifrancap@gmail.com>
2024-09-08 00:56:13 -0300

    Increased payload size to receive image bytes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cameraRoutes = require('./routes/cameras');
 const infractionRoutes = require('./routes/infractions');
 const infractionsStatsRoutes = require('./routes/infractionsStats');
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use('/', userRoutes);
 app.use('/', cameraRoutes);
 app.use('/', infractionRoutes);
