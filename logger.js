5e96180f305bf5a8e3836bab28cdda53350b3fb6
KaiquePerciliano <kaiqueperciliano374@gmail.com>
2024-09-11 22:18:16 -0300

    Remove log info

--- logger.js
+++ logger.js
@@ -2,20 +2,10 @@ const winston = require ('winston');
 
 const logger = winston.createLogger({
     format: winston.format.combine(
-        winston.format.errors({ stack: true }),
         winston.format.timestamp(),
         winston.format.json()
     ),
     transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'info.log', level: 'info' }),
     ],
 });
-
-if (process.env.NODE_ENV === 'test') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-}
-
-module.exports = logger;
