730c8070f1e5e920373227776705e20148ee473c
Andrei <andreifrancap@gmail.com>
2024-09-16 12:26:29 -0300

    added error log

--- routes/video.js
+++ routes/video.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/ConnectDatabase');
+const db = require('../config/connectDataBase');
 
 router.post('/video', (req, res) => {
     const { url } = req.body;
     const query = 'INSERT INTO video (video_url) VALUES (?)';
     db.query(query, [url], (err, result) => {
-        if (err) return res.status(500).json(err);
-        res.status(201).json({ url });
+        if (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+            res.status(201).json({ url });
         console.log("Usuário " +  + " enviado com sucesso!");
     });
 });
@@ -15,7 +18,11 @@ router.post('/video', (req, res) => {
 router.get('/video', (req,res)  => {
     const query = 'SELECT * FROM video';
     db.query(query, [], (err,result) => {
-        if (err) return res.status(500).json(err);
+        if (err){
+            console.log(err);
+            return res.status(500).json(err);
+        } 
+        console
         res.status(201).json( result[result.length -1] );
     });
 });
