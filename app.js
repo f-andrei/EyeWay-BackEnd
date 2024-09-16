7c3393a91c0d07ce72266bcb938f0a662f303c85
Andrei <andreifrancap@gmail.com>
2024-09-16 12:26:09 -0300

    removed duplicate line

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const infractionsStatsRoutes = require('./routes/infractionsStats');
 const videoRoutes = require('./routes/video');
 
 app.use(cors())
-app.use(express.json());
 app.use(express.json({limit: '50mb'}));
 app.use('/', userRoutes);
 app.use('/', cameraRoutes);
