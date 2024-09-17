8fe47654e3bebc22c6e2c66268c2fbe89ca93114
KaiquePerciliano <kaiqueperciliano374@gmail.com>
2024-09-17 19:25:04 -0300

    banco

--- config/connectDataBase.js
+++ config/connectDataBase.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
    host: "localhost",
    user: "root",
-   password: "senha1234",
+   password: "12082023Km@",
    database: "EyeWay"
 });
 
