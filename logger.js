a9d78a04460baba1913fd4ff7ce489fda7d52623
KaiquePerciliano <kaiqueperciliano374@gmail.com>
2024-10-12 02:21:18 -0300

    add stacktrace for logs

--- logger.js
+++ logger.js
@@ -3,9 +3,13 @@ const winston = require ('winston');
 const logger = winston.createLogger({
     format: winston.format.combine(
         winston.format.timestamp(),
+        winston.format.errors({ stack: true }),
         winston.format.json()
     ),
     transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'info.log', level: 'info' }),
     ],
 });
+
+module.exports = logger;
