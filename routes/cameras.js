23fe40710b9525f771d99e7643881d5d8cc848da
KaiquePerciliano <kaiqueperciliano374@gmail.com>
2024-10-12 02:23:27 -0300

    add  error handling Camera

--- routes/cameras.js
+++ routes/cameras.js
@@ -2,18 +2,26 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/connectDataBase');
 const logger = require('../logger');
+const { camera_id } = require('../sampleBase64');
 
 
 router.post('/cameras', (req, res) => {
     const { camera_id, location, ip_address, status } = req.body;
+
+    if (!camera_id || !location || !ip_address || !status) {
+        logger.info('Todos os campos são obrigatórios: camera_id, location, ip_address,  status.');
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios: camera_id, location, ip_address, status.' });
+    }
+
     const query = 'INSERT INTO cameras (camera_id, location, ip_address, status) VALUES (?, ?, ?, ?)';
+
     db.query(query, [camera_id, location, ip_address, status], (err, result) => {
         if (err) {
             logger.error(err);
-            return res.status(500).json(err);
+            return res.status(500).json({ message: 'Erro ao criar a câmera.' });
         }
-        res.status(201).json({ camera_id: result.insertId });
-        console.log("Câmera " + ip_address + " adicionada com sucesso!");
+        logger.info("Câmera " + id + " criada com sucesso!");
+        res.status(201).json({ id: result.insertId });
     });
 });
 
@@ -22,10 +30,10 @@ router.get('/cameras', (req, res) => {
     db.query(query, (err, results) => {
         if (err) {
             logger.error(err);
-            return res.status(500).json(err);
+            return res.status(500).json({ message: 'Erro ao listar as câmeras.' });
         }
+        logger.info('Câmeras listadas com sucesso!');
         res.status(200).json(results);
-        console.log("Câmeras listadas com sucesso!");
     });
 });
 
@@ -35,26 +43,31 @@ router.get('/cameras/:id', (req, res) => {
     db.query(query, [id], (err, result) => {
         if (err) {
             logger.error(err);
-            return res.status(500).json(err);
+            return res.status(500).json({message: 'Erro ao listar a câmera' + camera_id + '.'});
         }
         if (result.length === 0) return res.status(404).json({ message: 'Camera not found' });
+
+        logger.info('Câmera listada com sucesso!');
         res.status(200).json(result[0]);
-        console.log("Câmera " + id + " listada com sucesso!");
     });
 });
 
 router.put('/cameras/:id', (req, res) => {
     const { id } = req.params;
     const { camera_id, location, ip_address, status } = req.body;
-    const query = 'UPDATE cameras SET camera_id, location = ?, ip_address = ?, status = ? WHERE camera_id = ?';
+
+    if (!camera_id || !location || !ip_address || !status) {
+        logger.info('Todos os campos são obrigatórios: camera_id, location, ip_address,  status.');
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios: camera_id, location, ip_address,  status.' });
+    }
+    const query = 'UPDATE cameras SET camera_id = ?, location = ?, ip_address = ?, status = ? WHERE camera_id = ?';
     db.query(query, [camera_id, location, ip_address, status, id], (err, result) => {
         if (err) {
             logger.error(err);
-            return res.status(500).json(err);
+            return res.status(500).json({ message: 'Erro ao atualizar a câmera.' });
         }
-        res.status(200).json({ message: 'Camera updated' });
-        console.log("Câmera " + id + " atualizada com sucesso!");
-    });
+        logger.info("Câmera " + id + " atualizada com sucesso!");
+        res.status(200).json({ message: 'Camera ' + id + ' updated' });    });
 });
 
 router.delete('/cameras/:id', (req, res) => {
@@ -63,10 +76,10 @@ router.delete('/cameras/:id', (req, res) => {
     db.query(query, [id], (err, result) => {
         if (err) {
             logger.error(err);
-            return res.status(500).json(err);
+            return res.status(500).json({ message: 'Erro ao deletar a câmera.' });
         }
-        res.status(200).json({ message: 'Camera deleted' });
-        console.log("Câmera " + id + " deletada com sucesso!");
+        logger.info("Câmera " + id + " deletada com sucesso!");
+        res.status(200).json({ message: 'Camera '+ id + ' deleted' });
     });
 });
 
